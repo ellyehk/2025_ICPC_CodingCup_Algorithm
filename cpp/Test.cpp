a811b881b834bc46bfd53567fb49a4890b199f58
ellyehk <wiwi843up@gmail.com>
2025-01-21 18:19:10 -0600

    ICPC initial repository local

---
+++ cpp/Test.cpp
@@ -0,0 +1,15 @@
+/*Código Personalizado por Leslye Hernandez Jimenez
+ * Fecha: 20/01/2025
+ *        /\_/\
+ *       ( o.o )    
+ *        > ^ <
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello, programmers!!!"<<endl;
+
+    return 0;
+}
---
+++ java/Test.java
@@ -0,0 +1,14 @@
+/*Código Personalizado por Leslye Hernandez Jimenez
+ * Fecha: 20/01/2025
+ *        /\_/\
+ *       ( o.o )    
+ *        > ^ <
+ */
+
+
+public class Test {
+
+public static void main (String [] args){
+    System.out.println("Hello, programmers!!!");
+    }
+}
